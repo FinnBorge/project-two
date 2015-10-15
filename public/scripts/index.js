d92d922e031bd23b64533c26ee39b533d6168fae
Finn Borge <finnborge@gmail.com>
2015-10-15 17:16:14 -0400

    category hiding complete

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,26 +9,9 @@ var $containers = $('.index-container'),
 
 $index.on('click', '.index-category', function(e){
   if (notHidden) {
-    $(this).siblings().hide();
+    $(this).parent().siblings().hide("slow"); //siblings receive display = 'none'
   } else {
-    $(this).siblings().show();
+    $(this).parent().siblings().show("slow");
   }
-
   notHidden = !notHidden;
-
-  // if(notHidden){
-  //   var target = e.target; //click a .index-category
-  //   var targetParent = $(target).parent(); //hold its parent container
-  //   var position = $.inArray(target, $categories); //the position of the clicked inside the array of categories
-  //   if(position !== -1){
-  //     var hideContainers = $containers;
-  //     hideContainers.splice(position, 1);
-  //     var $hideThese = $(hideContainers);
-  //     $hideThese.toggleClass('hide-these');
-  //     notHidden = false;
-  //   }
-  // } else if (!notHidden) {
-  //   $containers.attr("class", "index-container");
-  //   notHidden = true;
-  // }
-})
+});
